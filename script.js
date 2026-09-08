67b1bb7e551b76ba8f4637692adc75f487dc38cc
shanandmadu143-glitch <shanandmadu143@gmail.com>
2026-09-08 19:09:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const display = document.getElementById('display');
+
+// ඩිස්ප්ලේ එකට අංක හෝ ලකුණු එකතු කිරීම
+function appendValue(input) {
+    display.value += input;
+}
+
+// ඩිස්ප්ලේ එක සම්පූර්ණයෙන්ම clear කිරීම
+function clearDisplay() {
+    display.value = '';
+}
+
+// අවසානයට යොදපු අංකය/ලකුණ ඉවත් කිරීම
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// උත්තරය တွက်တွက် කිරීම
+function calculateResult() {
+    try {
+        if (display.value !== '') {
+            display.value = eval(display.value);
+        }
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
 
